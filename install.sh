ccbefdecf2b348324cdcf18246bd6947a63a5e50
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-12 16:17:03 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+echo
 apt-get -y install -qq wget
 wget -qq https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -q -y ./google-chrome-stable_current_amd64.deb
